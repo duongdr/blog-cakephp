fa1c2977bf4db375be4dbc0f3d5c9f09d14dce38
duong <duongdr>
2021-01-15 10:27:56 +0700

    user role

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -17,7 +17,7 @@ class PostsController extends AppController {
 
 	public function beforeFilter() {
 		parent::beforeFilter();
-		$this->Auth->allow('index', 'view');
+//		$this->Auth->allow('index', 'view');
 	}
 
 	/**
